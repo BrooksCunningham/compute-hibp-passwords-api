9653165a82e572ffacb632732e4d0e73eb91ec7e
Brooks Cunningham <bcunningham@fastly.com>
2023-02-09 09:44:45 -0500

    Update main.rs

---
+++ add_files_to_object_store.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#curl -XPUT  "https://api.fastly.com/resources/stores/object/$OBJECTSTORE/keys/00000" \
+#    -H "Fastly-Key: $FASTLYPERSONALTOKEN" -H "Accept: application/json" \
+#    -d @./hibp_object_store/00000.txt
+
+## Ths is needed to add each file to it's own text file.
+#awk '{F=substr($0,1,5)".txt";print >> F;close(F)}' ~/Documents/mygit/compute-hibp-passwords-api/pwned-passwords-sha1-ordered-by-hash-v8.txt
+
+# Testing
+# curl https://hibp-api.edgecompute.app/range/00000
+
+
+# Add files to ECP API
+upload() {
+  OBJECTSTORE=""
+  for filename in $1*.txt; do
+    OBJKEY=(`basename $filename | cut -c 1-5`)
+    #echo "key to upload: " $OBJKEY
+    #echo "filename to upload " "$filename"
+
+    mv $filename processed/$filename
+
+    curl -XPUT  "https://api.fastly.com/resources/stores/object/$OBJECTSTORE/keys/$OBJKEY" \
+       -H "Fastly-Key: $FASTLYPERSONALTOKEN" \
+       -H "Accept: application/json" \
+       -H "if-generation-match:0"
+       --data-binary "@processed/$filename"
+
+  done
+}
+
